49bdc5d82c47be08e0c82dfe4bb2b8d012cd650a
Vedant Parikh <parikhvedant2003@gmail.com>
2025-09-08 21:07:01 +0530

    Deployment Path Changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-const basePath = "/";
+const basePath = process.env.NODE_ENV === "production" ? "/ParikhVedant2K3/" : "/";
 
 export default defineConfig({
   base: basePath,
